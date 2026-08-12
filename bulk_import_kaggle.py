b6662a06702b2d833f3799e6c5714754a868a7ee
MaxPowersPro <maxpowersbigfoothunter@gmail.com>
2026-08-12 08:53:28 -0400

    Update DATA_URL to include raw=true parameter

--- bulk_import_kaggle.py
+++ bulk_import_kaggle.py
@@ -3,7 +3,7 @@ import streamlit as st
 from supabase import create_client
 
 # Direct web source for the geocoded BFRO dataset
-DATA_URL = "https://raw.githubusercontent.com/timothyrenner/bfro_sightings_data/master/data/bfro_reports_geocoded.csv"
+DATA_URL = "https://raw.githubusercontent.com/timothyrenner/bfro_sightings_data/master/data/bfro_reports_geocoded.csv?raw=true"
 
 def run_import():
     st.write("🚀 Starting bulk import from web source...")
