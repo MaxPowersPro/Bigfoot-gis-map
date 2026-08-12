240046bb8107ab3202c67e3cee1d4787858fa185
MaxPowersPro <maxpowersbigfoothunter@gmail.com>
2026-08-12 08:48:13 -0400

    Correct bulk_import_kaggle import syntax
    Fixed import statement for bulk_import_kaggle.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import bulk_import_kaggle
-bulk_import_kaggle.run_import()
+import bulk_import_kagglebulk_import_kaggle.run_import()
 import streamlit as st
 import folium
 from streamlit_folium import st_folium
